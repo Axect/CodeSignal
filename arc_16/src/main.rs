a57b24dbc5a38c9ee9bfa5c196ac8f025772460f
axect <edeftg@gmail.com>
2019-03-12 00:14:17 +0900

    Arc 16 - Ver 2

--- arc_16/src/main.rs
+++ arc_16/src/main.rs
@@ -1,70 +1,45 @@
 fn main() {
-    println!("{}", areSimilar(vec![2,3,1], vec![1,3,2]));
+    println!("{}", areSimilar(vec![1,3,2,2,4], vec![1,3,2,4,2]));
 }
 
-fn areSimilar(a: Vec<i32>, b: Vec<i32>) -> bool {                               // [2,3,1], [1,3,2]
-    let mut iter_a = a.into_iter();
-    let mut iter_b = b.into_iter();
-    let mut swapped = false;
-    let mut duplicated = false;
-    let mut already_compared_a = std::i32::MIN;
-    let mut already_compared_b = std::i32::MIN;
+fn areSimilar(a: Vec<i32>, b: Vec<i32>) -> bool {
+    are_similar_stack(a, b, 0)
+}
 
-    loop {
-        match (iter_a.next(), iter_b.next()) {                                  // Some(2), Some(1) -> Some(3), Some(3) -> (None, None)
-            (Some(cmp_a), Some(cmp_b)) => {                                     // Some(2), Some(1) -> Some(3), Some(3)
-                if cmp_a == cmp_b {                                             // skip             -> true
-                    continue;                                                   // skip             -> continue
-                } else if cmp_a == already_compared_a && cmp_b == already_compared_b {
-                    if duplicated {
-                        return false;
-                    } else {
-                        duplicated = true;
-                    }
-                    continue;
-                } else {
-                    if swapped {                                                // skip
-                        return false;                                           // skip
-                    } else {
-                        let mut count = 0usize;                                 // count = 0
+fn are_similar_stack(a: Vec<i32>, b: Vec<i32>, stack: usize) -> bool {
+    if stack >= 2 {
+        return false;
+    }
+    
+    let mut a_iter = a.clone().into_iter();
+    let mut b_iter = b.clone().into_iter();
+    
+    match (a_iter.next(), b_iter.next()) {
+        (None, _) => true,
+        (_, None) => true,
+        (Some(cmp_a), Some(cmp_b)) if cmp_a == cmp_b => are_similar_stack(a_iter.collect::<Vec<i32>>(), b_iter.collect::<Vec<i32>>(), stack),
+        (Some(cmp_a), Some(cmp_b)) => {
+            let mut a_iter_cloned = a_iter.clone();
+            let mut b_iter_cloned = b_iter.clone();
+            let mut count = 0usize;
+            
+            loop {
+                match (a_iter_cloned.next(), b_iter_cloned.next()) {
+                    (None, _) => return false,
+                    (_, None) => return false,
+                    (Some(x), Some(y)) if x == cmp_b && y == cmp_a => {
+                        let mut a_left = a_iter.collect::<Vec<i32>>();
+                        let mut b_left = b_iter.collect::<Vec<i32>>();
+                        a_left.remove(count);
+                        b_left.remove(count);
                         
-                        let mut iter_a_cloned = iter_a.clone();
-                        let mut iter_b_cloned = iter_b.clone();
-                        
-                        loop {
-                            match iter_b_cloned.next() {                        // Some(3) -> Some(2)
-                                Some(is_cmp_a) => {                             // Some(3) -> Some(2)
-                                    count += 1;                                 // count=1 -> count=2                      
-                                    if cmp_a == is_cmp_a {                      // false   -> true
-                                        already_compared_b = cmp_a;
-                                        for i in 0 .. count - 1 {               //         -> for i in 0 .. 1
-                                            iter_a_cloned.next();               //         -> Some(3)
-                                        }
-                                        match iter_a_cloned.next() {            //         -> Some(1)
-                                            Some(is_cmp_b) => {                 //         -> Some(1)
-                                                if is_cmp_b == cmp_b {          //         -> Some(1) == Some(1)
-                                                    swapped = true;             //         -> swapped = true
-                                                    already_compared_a = cmp_b;
-                                                    break;                      //         -> break
-                                                } else {
-                                                    return false;
-                                                }
-                                            },
-                                            None => return false,
-                                        }
-                                    } else {
-                                        continue;                               // continue
-                                    }
-                                },
-                                None => return false,
-                            }
-                        }
+                        return are_similar_stack(a_left, b_left, stack + 1);
+                    },
+                    (Some(x), Some(y)) => {
+                        count += 1;
                     }
                 }
-            },
-            (None, _) => break,                                                 //
-            (_, None) => break,
+            }
         }
     }
-    true
 }
